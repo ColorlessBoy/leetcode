66256af2306c958cc79c8137c20d2fa2ca15e19e
penglingwei <penglingwei@zju.edu.cn>
2020-08-23 12:34:07 +0800

    动态规划:day4

---
+++ 730.统计不同回文子序列.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=730 lang=cpp
+ *
+ * [730] 统计不同回文子序列
+ */
+/*
+ * Note:
+ * 这个动态规划问题有点难, 暴力的话最高复杂度4^500，关键是我们要找到子问题。
+ * /
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int countPalindromicSubsequences(string S) {
+
+    }
+};
+// @lc code=end
+
+int main(int argc, char **argv) {
+    Solution s;
+
+    return 0;
+}
+
